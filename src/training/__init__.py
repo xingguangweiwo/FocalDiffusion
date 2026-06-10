eeda4936ffb8309892643b9fd47ce04cdc19219b
Zhuojun Zhou <103349716+xingguangweiwo@users.noreply.github.com>
2026-06-11 02:28:17 +0800

    Remove legacy trainer export

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,14 +1,11 @@
-"""FocalStackGeneration training utilities"""
+"""FocalStackGeneration training utilities."""
 
-from .trainer import FocalDiffusionTrainer, FocalStackGenerationTrainer
-from .losses import (
-    FocalStackGenerationLoss,
-)
+from .trainer import FocalStackGenerationTrainer
+from .losses import FocalStackGenerationLoss
 from .optimizers import get_optimizer, get_scheduler
 
 __all__ = [
     "FocalStackGenerationTrainer",
-    "FocalDiffusionTrainer",
     "FocalStackGenerationLoss",
     "get_optimizer",
     "get_scheduler",
